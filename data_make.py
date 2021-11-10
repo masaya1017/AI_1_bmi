6e5974228fedc527668724b720318fb6eb7a5556
masaya1017 <82097439+masaya1017@users.noreply.github.com>
2021-11-10 17:55:33 +0900

    db_comment_ad

--- data_make.py
+++ data_make.py
@@ -5,12 +5,13 @@ db_path = "./hw.sqlite3"
 
 
 def insert_db(conn):
+    #身長と体重それぞれについてランダム生成を行う。
     height = random.randint(130, 180)
     weight = random.randint(30, 100)
-
+    #BMIの算出
     bmi = weight/(height/100)**2
     type_no = 1
-
+    #BMIの数値によって、カテゴリ分類を実行する。
     if bmi < 18.5:
         type_no = 0
     elif bmi < 25:
@@ -23,12 +24,14 @@ def insert_db(conn):
         type_no = 4
     else:
         type_no = 5
-
+    #sqlの発行(データの挿入処理,クエリパラメータ)
     sql = '''
         INSERT INTO person(height,weight,typeNo) VALUES (?,?,?)
     '''
+    #タプルの生成
     values = (height, weight, type_no)
     print(values)
+    #DB実行時は、リストに格納すること注意する。
     conn.executemany(sql, [values])
 
 
